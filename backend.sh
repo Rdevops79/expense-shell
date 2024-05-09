c6e009040519bfcd40e61c0fb694f6a41abe3204
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 15:56:30 +0530

    updating thge frontend

--- backend.sh
+++ backend.sh
@@ -1,6 +1,7 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
@@ -8,3 +9,10 @@ cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
+dnf install mysql -y
+mysql -h 54.242.161.57 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl restart backend
+
