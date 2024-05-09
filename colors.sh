8ef93e9c4bca05e11d8bbc3c7f9ec4abe2631a08
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 17:23:59 +0530

    updating thge frontend

--- colors.sh
+++ colors.sh
@@ -6,7 +6,7 @@
 ## \e [COLm  ; COL is color code , red(31) , green (32) , yellow (33) , blue (34) , magneta(35),cayn(36)
 ## \e[om  ; Disable the enable colo
 
- echo -e "\e[31m Hello World \e[om"
- echo -e "\e[32m MR. \e[om"
- echo -e "\e[33m Revanth sai \e[om"
- echo -e "\e[34m Yarlagadda \e[om"
+ echo -e "\e[31m Hello World\e[0m"
+ echo -e "\e[32m MR.\e[0m"
+ echo -e "\e[33m Revanth sai\e[0m"
+ echo -e "\e[34m Yarlagadda\e[0m"
