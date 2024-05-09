d8ff5711a0c93eb58ef6745ffe81f7ef2e69a63f
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 17:43:45 +0530

    updating thge frontend

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
- echo -e"\e[31m installing nginx\e[0m"
+ echo -e"\e[31minstalling nginx\e[0m"
  dnf install nginx -y
 
- echo -e"\e[32m coping expense config file\e[0m"
+ echo -e"\e[32mcoping expense config file\e[0m"
  cp expense.conf /etc/nginx/default.d/expense.conf
 
- echo -e"\e[33m cleaning old content\e[0m"
+ echo -e"\e[33mcleaning old content\e[0m"
  rm -rf /usr/share/nginx/html/*
 
- echo -e"\e[34m download front end content \e[0m"
+ echo -e"\e[34mdownload front end content \e[0m"
  curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
  cd /usr/share/nginx/html
 
- echo -e"\e[35m extracting the content \e[0m"
+ echo -e"\e[35mextracting the content \e[0m"
  unzip /tmp/frontend.zip
 
- echo -e"\e[36m restart service\e[0m"
+ echo -e"\e[36mrestart service\e[0m"
  systemctl restart nginx
  systemctl enable nginx
