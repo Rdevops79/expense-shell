efcebd3f086203ba32c72bdd2312cc29d625e30e
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 18:28:34 +0530

    updating thge frontend

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,26 @@
  echo -e "\e[31minstalling nginx\e[0m"
  dnf install nginx -y
+ echo exit status-$?
 
  echo -e "\e[32mcoping expense config file\e[0m"
  cp expense.conf /etc/nginx/default.d/expense.conf
+ echo exit status-$?
 
  echo -e "\e[33mcleaning old content\e[0m"
  rm -rf /usr/share/nginx/html/*
+echo exit status-$?
 
  echo -e "\e[34mdownload front end content \e[0m"
  curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
  cd /usr/share/nginx/html
+ echo exit status-$?
 
  echo -e "\e[35mextracting the content \e[0m"
  unzip /tmp/frontend.zip
+ echo exit status-$?
 
  echo -e "\e[36mrestart service\e[0m"
  systemctl restart nginx
  systemctl enable nginx
+  echo exit status-$?
 
