fdcdb0a1c6b0a6dc2f27efc9cdd4d22ff62c668b
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 13:47:13 +0530

    updating thge frontend

--- frontend.sh
+++ frontend.sh
@@ -6,5 +6,5 @@
  curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
  cd /usr/share/nginx/html
  unzip /tmp/frontend.zip
- # copy the file step pending do it later
+ cp expense.conf /etc/nginx/default.d/expense.conf
  systemctl restart nginx
