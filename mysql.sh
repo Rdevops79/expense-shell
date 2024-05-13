315203ecbc3dda692c260942b4c191161211fc4b
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-13 18:34:21 +0530

    updating thge frontend

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,11 @@
+echo -e "\e[31mInstalling MySQL server\e[0m"
 dnf install mysql-server -y
+echo exit status-$?
+echo -e "\e[31mStart Nginx Server\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo exit status-$?
+echo -e "\e[31mSet Root Password\e[0m"
+mysql_secure_installation --set-root-pass $1
+## PASSWORD-ExPensse@1
+echo exit status-$?
