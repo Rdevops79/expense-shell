fdc5be3c27e3232aeeb62adfd928304d778476e6
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-15 23:30:02 +0530

    updating thge frontend

--- frontend.sh
+++ frontend.sh
@@ -3,28 +3,28 @@ rm -f /tmp/expense.log
 
  HEADING installing nginx
  dnf install nginx -y &>>/tmp/expense.log
- echo exit status-$?
+STAT $?
 
  HEADING coping expense config file
  cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
- echo exit status-$?
+ STAT $?
 
  HEADING cleaning old content
  rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
-echo exit status-$?
+ STAT $?
 
  HEADING download front end content
  curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
- echo exit status-$?
+ STAT $?
 
 cd /usr/share/nginx/html
 
  HEADING extracting the content
  unzip /tmp/frontend.zip &>>/tmp/expense.log
- echo exit status-$?
+ STAT $?
 
  HEADING restart service
  systemctl restart nginx &>>/tmp/expense.log
  systemctl enable nginx &>>/tmp/expense.log
-  echo exit status-$?
+ STAT $?
 
